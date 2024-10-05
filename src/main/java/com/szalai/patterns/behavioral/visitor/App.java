268b6f1fdd187626e7c27cb2f84436809b3380f6
Gergo Szalai <szerg97@gmail.com>
2024-10-05 16:42:28 +0200

    visitor pattern

--- src/main/java/com/szalai/patterns/behavioral/visitor/App.java
+++ src/main/java/com/szalai/patterns/behavioral/visitor/App.java
@@ -1,8 +1,14 @@
 package com.szalai.patterns.behavioral.visitor;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        Book book = new Book("Lord of the Rings", 150);
-        book.accept(new ShoppingCartVisitor());
+        List<Book> books = List.of(
+                new Book("Lord of the Rings", 150),
+                new Book("Star Wars", 190)
+        );
+        Visitor visitor = new ShoppingCartVisitor();
+        books.forEach(book -> book.accept(visitor));
     }
 }
